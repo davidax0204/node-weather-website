9fa21d34391a56d3c7f31bdee1000ca77e841e6d
davidax0204 <davidax5625@gmail.com>
2021-03-11 16:48:39 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -35,7 +35,7 @@ weatherForm.addEventListener('submit', (e)=>
     messageTwo.textContent =''
 
     const location = serach.value
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>
+    fetch('/weather?address='+location).then((response)=>
     {
         response.json().then((data)=>
         {
